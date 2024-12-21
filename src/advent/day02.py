65cdd0c0d016838cb2c1551a1a3360432731a6fb
camhpj <cjohnson23@gwu.edu>
2024-12-21 14:01:44 -0500

    working on day 2

--- src/advent/day02.py
+++ src/advent/day02.py
@@ -2,6 +2,9 @@ from advent.solution import Solution
 
 
 class Day02(Solution):
+    def preprocess(self) -> list[list[int]]:
+        return [list(map(int, line.split(" "))) for line in self.input.split("\n")]
+
     def solve_part1(self) -> int:
         return 0
 
--- tests/test_solutions.py
+++ tests/test_solutions.py
@@ -3,13 +3,13 @@ from advent.day02 import Day02
 from advent.solution import load_input
 
 
-def test_Day1() -> None:
+def test_Day01() -> None:
     prob = Day01(load_input("tests/data/day01.txt"))
     assert prob.solve_part1() == 11
     assert prob.solve_part2() == 31
 
 
-def test_Day2() -> None:
+def test_Day02() -> None:
     prob = Day02(load_input("tests/data/day01.txt"))
     assert prob.solve_part1() == 0
     # assert prob.solve_part2()

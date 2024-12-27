1c208aa48a4fe3670d9b330ffedccb16aefd286e
camhpj <cjohnson23@gwu.edu>
2024-12-27 14:27:15 -0500

    solve day05 part2

--- src/advent/day05.py
+++ src/advent/day05.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from typing import DefaultDict
+
 from advent.solution import Solution
 
 
@@ -13,19 +14,25 @@ class Day05(Solution):
             map_[i].append(j)
         return (map_, page_order)
 
+    def update_indices(self, num: int, i: int, idx: dict[int, int]) -> dict[int, int]:
+        if i in idx.values():
+            for k, v in idx.items():
+                if v <= i:
+                    idx[k] = v - 1
+        idx[num] = i if i != 1e8 else max(idx.values()) + 1
+        return idx
+
     def solve_line(self, line: list[int], map_: DefaultDict[int, list[int]]) -> tuple[dict[int, int], bool]:
         idx = {line[0]: 0}
         for num in line[1:]:
-            num_idx = 1e8
+            i = 1e8
             for el in map_[num]:
                 if el not in idx.keys():
                     continue
                 tmp = idx[el] - 1
-                if tmp <= num_idx:
-                    num_idx = tmp
-            if num_idx == 1e8:
-                num_idx = max(idx.values()) + 1
-            idx[num] = num_idx
+                if tmp <= i:
+                    i = tmp
+            idx = self.update_indices(num, i, idx)
         in_order = line == [j for _, j in sorted(zip(idx.values(), idx.keys()))]
         return (idx, in_order)
 

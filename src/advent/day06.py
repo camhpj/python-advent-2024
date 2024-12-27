33ae63dc7a912ac7a5eaf0b653d34928874bcf1d
camhpj <cjohnson23@gwu.edu>
2024-12-27 15:50:00 -0500

    working on day06 part1

--- src/advent/day06.py
+++ src/advent/day06.py
@@ -1,9 +1,43 @@
+from itertools import cycle
+
+import numpy as np
+
 from advent.solution import Solution
 
 
 class Day06(Solution):
+    def preprocess(self) -> np.ndarray:
+        return np.array([[*i] for i in self.input.split("\n")])
+
     def solve_part1(self) -> int:
-        return 0
+        map_ = self.preprocess()
+        i, j = np.ravel(np.where(map_ == "^"))
+        for point in cycle(["N", "E", "S", "W"]):
+            d = 1
+            prev = i, j
+            while True:
+                match point:
+                    case "N":
+                        x, y = i - d, j
+                    case "E":
+                        x, y = i, j + d
+                    case "S":
+                        x, y = i + d, j
+                    case "W":
+                        x, y = i, j - d
+                    case _:
+                        raise ValueError()
+                d += 1
+                try:
+                    if map_[x, y] == "#":
+                        i, j = prev
+                        break
+                    else:
+                        map_[prev] = "X"
+                        map_[x, y] = "^"
+                        prev = x, y
+                except IndexError:
+                    return np.where(map_ == "X")[0].shape[0] + 1
 
     def solve_part2(self) -> int:
         return 0
